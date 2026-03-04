5cf20aa57be2fe8bbb9128b5b0fca8aef2965276
lucadani7 <79052990+lucadani7@users.noreply.github.com>
2026-03-04 10:20:46 +0200

    Refactor `CustomColorConverter` to simplify switch case formatting in `getForegroundColorCode`.

--- Laboratoire1/src/main/java/utcb/fii/utils/CustomColorConverter.java
+++ Laboratoire1/src/main/java/utcb/fii/utils/CustomColorConverter.java
@@ -23,10 +23,10 @@ public class CustomColorConverter extends ForegroundCompositeConverterBase<ILogg
     protected String getForegroundColorCode(ILoggingEvent event) {
         Level level = event.getLevel();
         return switch (level.toInt()) {
-            case Level.ERROR_INT -> "31"; // foreground red
-            case Level.WARN_INT -> "38;5;208"; // dark orange
-            case Level.INFO_INT -> "32"; // foreground green
-            default -> "39"; // default color in IDE console
+            case Level.ERROR_INT -> "31";
+            case Level.WARN_INT -> "38;5;208";
+            case Level.INFO_INT -> "32";
+            default -> "39";
         };
     }
 }
